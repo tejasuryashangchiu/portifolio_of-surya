5ea5d92f82330a8f1aa161669d9b72a7c41253cf
Surya Teja <steja8310@gmail.com>
2025-07-17 20:54:57 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+   var typed = new Typed(".text", {
+      strings: ["Frontend Developer", "Full Stack Developer", "Web Developer"],
+      typeSpeed: 150,
+      backSpeed: 150,
+      backDelay: 1000,
+      loop: true
+    });
